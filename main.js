ce544fea757872aa4e274eac6510931be1a37182
quacoo <quacoo.boat@gmail.com>
2023-03-12 07:41:50 +0000

    add two displays

--- main.js
+++ main.js
@@ -3,8 +3,9 @@ import './style.css'
 const catsElement = document.querySelector('.cats');
 const displayElement = document.querySelector('#display');
 
-function getCatTemplate({name, imageURL}){
+function getCatTemplate({name, imageURL, clicks}){
   return `
+    <div id="display" class="display-board">${clicks}</div>
     <h4 class="cat-name">${name}</h4>
     <div id="cuteCat" class="cat-container">
       <img class="cat-image" src="${imageURL}" alt="cute cat" />
@@ -12,15 +13,14 @@ function getCatTemplate({name, imageURL}){
   ` 
 }
 const cats = [
-  { name: 'Adorable Cat', imageURL: "https://media.distractify.com/brand-img/cZYoJwx_U/0x0/iStock-910314172-1553686882394.jpg"},
-  { name: 'Sleeping Cat', imageURL: "https://www.animalfriends.co.uk/siteassets/media/images/article-images/cat-articles/afi_shutterstock_1808434111_article_why-do_cats_sleep_so_much.jpg"},
+  { name: 'Adorable Cat', imageURL: "https://media.distractify.com/brand-img/cZYoJwx_U/0x0/iStock-910314172-1553686882394.jpg", clicks: 0},
+  { name: 'Sleeping Cat', imageURL: "https://www.animalfriends.co.uk/siteassets/media/images/article-images/cat-articles/afi_shutterstock_1808434111_article_why-do_cats_sleep_so_much.jpg", clicks: 0},
 ]
-let count = 0;
 
 cats.forEach(cat => {
   const catElement = document.createElement('div');
   catElement.classList.add('cat');
   catElement.innerHTML = getCatTemplate(cat);
-  catElement.addEventListener('click', () => {  display.textContent = `${++count}`});
+  catElement.addEventListener('click', event => {catElement.querySelector('.display-board').textContent = `${++cat.clicks}`});
   catsElement.appendChild(catElement);
 });
