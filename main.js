20719244504ea6d74c28e6ab92574716f8ab4bd9
quacoo <quacoo.boat@gmail.com>
2023-03-12 06:45:38 +0000

    add two cats with names

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 import './style.css'
 
-const cuteCat = document.querySelector('#cuteCat');
-const display = document.querySelector('#display');
+const cuteCatElement = document.querySelectorAll('#cuteCat');
+const displayElement = document.querySelector('#display');
+const catNameElement = document.querySelectorAll('.cat-name');
 
+const catNames = ['Adorable Cat', 'Sleeping Cat'];
 let count = 0;
 
-cuteCat.addEventListener('click', () => {
-  display.textContent = `${++count}`;
-});
+catNameElement.forEach((element, index) => element.textContent = catNames[index]);
+cuteCatElement.forEach( 
+  element => element.addEventListener('click', () => {  display.textContent = `${++count}` })
+);
