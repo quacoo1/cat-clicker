811329cc346516df30d0142a0e75ed07674a9b70
quacoo <quacoo.boat@gmail.com>
2023-03-12 07:20:20 +0000

    recfactor two cats, two names

--- main.js
+++ main.js
@@ -1,13 +1,26 @@
 import './style.css'
 
-const cuteCatElement = document.querySelectorAll('#cuteCat');
+const catsElement = document.querySelector('.cats');
 const displayElement = document.querySelector('#display');
-const catNameElement = document.querySelectorAll('.cat-name');
 
-const catNames = ['Adorable Cat', 'Sleeping Cat'];
+function getCatTemplate({name, imageURL}){
+  return `
+    <h4 class="cat-name">${name}</h4>
+    <div id="cuteCat" class="cat-container">
+      <img class="cat-image" src="${imageURL}" alt="cute cat" />
+    </div>
+  ` 
+}
+const cats = [
+  { name: 'Adorable Cat', imageURL: "https://media.distractify.com/brand-img/cZYoJwx_U/0x0/iStock-910314172-1553686882394.jpg"},
+  { name: 'Sleeping Cat', imageURL: "https://www.animalfriends.co.uk/siteassets/media/images/article-images/cat-articles/afi_shutterstock_1808434111_article_why-do_cats_sleep_so_much.jpg"},
+]
 let count = 0;
 
-catNameElement.forEach((element, index) => element.textContent = catNames[index]);
-cuteCatElement.forEach( 
-  element => element.addEventListener('click', () => {  display.textContent = `${++count}` })
-);
+cats.forEach(cat => {
+  const catElement = document.createElement('div');
+  catElement.classList.add('cat');
+  catElement.innerHTML = getCatTemplate(cat);
+  catElement.addEventListener('click', () => {  display.textContent = `${++count}`});
+  catsElement.appendChild(catElement);
+});
